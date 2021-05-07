06117c3d07e968c1a39ac48f414b048c58838f09
rodrigo-calle <rodrigoc_0@hotmail.com>
2021-05-07 11:22:31 -0500

    registro de prodcutos finalizado v2.0

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,25 @@
 class Product < ApplicationRecord
     has_one_attached :product_img
     #validate :acceptable_image
+
+    
+    validates :category, presence: true 
+    validates :subcategory, presence: true 
+    validates :brand, presence: true 
+    validates :model, presence: true 
+    validates :condition, presence: true 
+    validates :description, presence: true 
+    validates :price, presence: true 
+    validates :unit, presence: true 
+    validates :color, presence: true 
+    validates :supplier, presence: true 
+    validates :stock, presence: true 
+    validates :material, presence: true 
+    validates :product_img, presence: true 
+    validates :characteristic, presence: true 
+
+
+
+
+
 end
